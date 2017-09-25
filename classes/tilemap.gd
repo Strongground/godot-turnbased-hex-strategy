98f113195e77bcd6b2b0e62b74008eebe407929d
Noël Ströhmer-Lohfink <n.stroehmer@engelhorn.de>
2017-09-25 16:03:57 +0200

    delete old version of scene, populated tilemap-class script, played around with tiles, tilesets and tilemaps and the necessary structure to add attributes to each tile type.

--- classes/tilemap.gd
+++ classes/tilemap.gd
@@ -1,10 +1,52 @@
 extends TileMap
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+# class member variables
+var tile_types = {
+	0: {
+		'name': 'Plain Grass',
+		'move_cost': 1.0,
+		'terrain': 'land'
+	},
+	1: {
+		'name': 'Forest',
+		'move_cost': 5.0,
+		'terrain': 'land'
+	},
+	2: {
+		'name': 'River',
+		'move_cost': 10.0,
+		'terrain': 'river'
+	},
+	3: {
+		'name': 'Road',
+		'move_cost': 0.5,
+		'terrain': 'land'
+	},
+	4: {
+		'name': 'Mountains',
+		'move_cost': 12.0,
+		'terrain': 'land'
+	},
+	5: {
+		'name': 'Village',
+		'move_cost': 1.5,
+		'terrain': 'land'
+	},
+	6: {
+		'name': 'Water',
+		'move_cost': 1.0,
+		'terrain': 'water'
+	}
+}
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
+
+func _get_tile_type_by_index(index):
+	if index <= tile_types.size():
+		return tile_types[index]
+	else:
+		print("Error: tilemap.gd - No tile type with that index.")
+		return false
--- scenes/map.gd
+++ scenes/map.gd
@@ -5,6 +5,7 @@ var camera = null
 var globals = null
 var tiles = null
 var hexmap = null
+var tile = null
 
 func _ready():
 	set_fixed_process(true)
@@ -12,16 +13,31 @@ func _ready():
 	camera = find_node("MainCamera")
 	globals = get_node("/root/globals")
 	hexmap = get_node("MapZones")
-	print(hexmap)
 	tiles = _create_tile_list(hexmap)
 
 func _input(event):
-	if event.type == 1:
+	if event.type == InputEvent.KEY:
 		if event.scancode == KEY_ESCAPE:
 			get_tree().quit()
+	elif event.type == InputEvent.MOUSE_BUTTON:
+		print("Got Tile "+String(self.get_tile(event.pos))+" at position "+String(event.pos))
 
 func _fixed_process(delta):
 	pass
 	
 func _create_tile_list(tilemap):
-	print(tilemap.get_used_cells())
+	var myfile = File.new()
+	print('Start exporting tiles')
+	myfile.open("res://tiles.txt", 3)
+	myfile.store_string(String(tilemap.get_used_cells()))
+	myfile.close()
+	print('Done storing')
+
+func get_tile(world_position):
+	var grid_pos = hexmap.world_to_map(world_position)
+	var tile = {
+		'index': hexmap.get_cell(grid_pos.x, grid_pos.y),
+		'attributes': hexmap._get_tile_type_by_index(hexmap.get_cell(grid_pos.x, grid_pos.y)),
+		'position': grid_pos
+	}
+	return tile
