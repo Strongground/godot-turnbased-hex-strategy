dc3dd79188915cc51c9469b575aeadf248759fc0
Noël Ströhmer-Lohfink <noel@strongground.de>
2017-09-26 07:57:11 +0200

    Added dynamic world position label for every tile (now make it toggelable!)

--- scenes/map.gd
+++ scenes/map.gd
@@ -5,7 +5,8 @@ var camera = null
 var globals = null
 var tiles = null
 var hexmap = null
-var tile = null
+var marker = null
+var root = null
 
 func _ready():
 	set_fixed_process(true)
@@ -13,18 +14,22 @@ func _ready():
 	camera = find_node("MainCamera")
 	globals = get_node("/root/globals")
 	hexmap = get_node("MapZones")
-	tiles = _create_tile_list(hexmap)
+	marker = get_node("RedDot")
+	root = get_node("/root")
+	self.display_hex_id()
 
 func _input(event):
 	if event.type == InputEvent.KEY:
 		if event.scancode == KEY_ESCAPE:
 			get_tree().quit()
 	elif event.type == InputEvent.MOUSE_BUTTON:
+		marker.set_pos(event.pos)
 		print("Got Tile "+String(self.get_tile(event.pos))+" at position "+String(event.pos))
 
 func _fixed_process(delta):
 	pass
 	
+# Debug method to write every tile and its tilemap index into a file for checking
 func _create_tile_list(tilemap):
 	var myfile = File.new()
 	print('Start exporting tiles')
@@ -33,11 +38,33 @@ func _create_tile_list(tilemap):
 	myfile.close()
 	print('Done storing')
 
+# Debug method to return a tiles attributes as defined in tilemap.gd
 func get_tile(world_position):
+	# Calculate grid position from world position
 	var grid_pos = hexmap.world_to_map(world_position)
-	var tile = {
-		'index': hexmap.get_cell(grid_pos.x, grid_pos.y),
-		'attributes': hexmap._get_tile_type_by_index(hexmap.get_cell(grid_pos.x, grid_pos.y)),
-		'position': grid_pos
-	}
+	# Get tile attributes based on tileset index
+	var tile = hexmap._get_tile_type_by_index(hexmap.get_cell(grid_pos.x, grid_pos.y))
+	# Enrich the returned tile object for debugging purposes
+	tile.index = hexmap.get_cell(grid_pos.x, grid_pos.y)
+	tile.position = grid_pos
 	return tile
+	
+# Debug method to draw around the region of a tile to highlight it
+func highlight_hex(position):
+	# get hexagon from world_position
+	pass
+
+func display_hex_id():
+	var tile_world_pos = null
+	for tile_x in range(-3, 26):
+		for tile_y in range(-7, 10):
+			# print("["+String(tile_x)+","+String(tile_y)+"]")
+			tile_world_pos = hexmap.map_to_world(Vector2(tile_x, tile_y))
+			print("tile_world_pos set: "+String(tile_world_pos))
+			# self.draw_circle(tile_world_pos, hexmap.get_cell_size().x, Color(globals.getColor('red')[0],globals.getColor('red')[1], globals.getColor('red')[2]))
+			var new_node = Label.new()
+			new_node.set_text(String(tile_world_pos))
+			new_node.set_pos(tile_world_pos)
+			self.add_child(new_node)
+			print("Attempted to add_child to map and now setting owner")
+			new_node.set_owner(get_tree().get_edited_scene_root())
